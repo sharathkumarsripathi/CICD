19762e07276b35e58023e8383506de849639c1ba
sharathkumarsripathi <sharathkumarsripathi@gmail.com>
2021-04-05 16:00:30 +0530

    sample testing.

--- src/main/java/com/javatechie/aws/cicd/example/OrderDao.java
+++ src/main/java/com/javatechie/aws/cicd/example/OrderDao.java
@@ -12,10 +12,10 @@ public class OrderDao {
 
     public List<Order> getOrders() {
         return Stream.of(
-                new Order(101, "Mobile", 1, 30000),
-                new Order(58, "Book", 4, 2000),
-                new Order(205, "Laptop", 1, 150000),
-                new Order(809, "headset", 1, 1799))
+                new Order(101, "SHARATH", 1, 30000),
+                new Order(58, "KUMAR", 4, 2000),
+                new Order(205, "INTENSE", 1, 150000),
+                new Order(809, "FORTUNE", 1, 1799))
                 .collect(Collectors.toList());
     }
 }
